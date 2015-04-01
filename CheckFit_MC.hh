0885b1bc675025ebe0860b760567fdc6a35f69d6
Lea Jouvin <ljouvin@ccage007.in2p3.fr>
2015-04-01 10:42:31 +0200

    checkfit bien commenté pour le cvs

--- CheckFit_MC.hh
+++ CheckFit_MC.hh
@@ -25,10 +25,11 @@
 
 namespace START {
   /**
-   *This class is used to remplace the ON of the Data by a poissonian distribution in each bands (zenith, offset, area and energy).
+   *This class is used to remplace the ON and OFF Data by a poissonian distribution in each bands (zenith, offset, area and energy).
    *The mean of the ON in each Band is obtained by calculating the mean of the signal and the mean of the background in the ON.
-   *The mean of the signal is calculated with the functionExpectedExcess of the computeResult Class in using the fittedparameters of the law used in Startfit.
-   *the mean of the background in each band is calculated by using the formula: alpha_iband * N_OFF
+   *The mean of the signal is calculated with the functionExpectedExcess of the computeResult Class by usinf the spectral parameters of the law generating the MCs given as an input
+   *the mean of the background in each band is calculated by using the formula: alpha_iband * N_OFF for E<1Tev and a power law of spectral index 2.7 for energy >1 TeV.
+   *The OFF data are not changed for E<1 TeV. For E>1TeV, the off data are calculated by drawing a poissonian law with a mean value for each energy bin determined by the integration of the power law with a spectral index of 2.7 between each boundaries of the bin.
    */ 
   class CheckFit_MC 
   {
